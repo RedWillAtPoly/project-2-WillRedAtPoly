7e030cb42a9459a1fb3dc322faf414169e5739dc
Will Red Poly <wredekop@calpoly.edu>
2026-02-09 22:43:26 +0000

    Functions written, require refinement

--- p2.py
+++ p2.py
@@ -11,24 +11,41 @@ WORDS = [
 
 # ----- Core scoring logic -----
 def score_guess(guess, secret):
-    pass
+    output = []
+    [x for x in secret]
+    for i in range(5):
+        if guess[i] == secret[i]:
+            output.append('Y')
+        elif guess[i] in secret:
+            output.append('O')
+        else:
+            output.append('X')
+    return ''.join(output)
 
 
-# ----- Check guess formatting -----
-def is_valid_guess(guess):
+# ----- Check guess formatting ----- Checks for 5 character all lowercase string
+def is_valid_guess(guess: str) -> bool:
+    if len(guess) != 5:
+        return False
+    for i in range(5):
+       if ord(guess[i]) < 97 or ord(guess[i]) > 122:
+           return False
+    return True
     
 
-# ----- Choose secret word -----
+# ----- Choose secret word ----- Chooses first element of a list of words
 def choose_secret(words):
-   pass
+   return words[0]
 
 
 
 
 
 # ----- One turn of the game -----
-def play_turn(secret, guess):
-    pass
+def play_turn(secret: str, guess: str) ->str:
+    if not is_valid_guess(guess):
+        return 'Invalid guess'
+    return score_guess(guess, secret)
 
 
 # ----- Full Wordle game -----
